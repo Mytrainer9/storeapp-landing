c77559b7a4f06a11363fb179162f0b947b3d2999
Mytrainer9 <mytrainer9@gmail.com>
2026-04-09 09:47:37 +0800

    remove: Features, LogoStrip, ContentBlocks, Integrations, Testimonials, Stats sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
 "use client";
 
-import Navbar        from "@/components/Navbar";
-import Hero          from "@/components/Hero";
-import MeetSection   from "@/components/MeetSection";
-import MarqueeStrip  from "@/components/MarqueeStrip";
-import Features      from "@/components/Features";
-import LogoStrip     from "@/components/LogoStrip";
-import ContentBlocks from "@/components/ContentBlocks";
-import Integrations  from "@/components/Integrations";
-import Testimonials  from "@/components/Testimonials";
-import Stats         from "@/components/Stats";
-import Footer        from "@/components/Footer";
+import Navbar       from "@/components/Navbar";
+import Hero         from "@/components/Hero";
+import MeetSection  from "@/components/MeetSection";
+import MarqueeStrip from "@/components/MarqueeStrip";
+import Footer       from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -19,12 +13,6 @@ export default function Home() {
       <Hero />
       <MeetSection />
       <MarqueeStrip />
-      <Features />
-      <LogoStrip />
-      <ContentBlocks />
-      <Integrations />
-      <Testimonials />
-      <Stats />
       <Footer />
     </main>
   );
